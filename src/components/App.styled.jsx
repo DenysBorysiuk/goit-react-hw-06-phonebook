c0fe1fc7d56e5551a3e688a83b650448c5dfbc57
Denys Borysiuk <denisborisiuk@gmail.com>
2023-02-16 19:57:10 +0200

    fix title margin

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -3,7 +3,6 @@ import styled from '@emotion/styled';
 export const SubTitle = styled.h2`
   display: inline-block;
   padding: 4px 16px;
-  margin-bottom: 20px;
   border-radius: ${props => props.theme.borderRadius};
   box-shadow: ${props => props.theme.boxShadow.Out};
 `;
